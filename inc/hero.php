6a94f4d5627b30ae9f032a6f88f732f6a6e298df
Collins Wagaba <wagabaplus256@gmail.com>
2015-08-13 20:00:35 +0300

    linking

--- inc/hero.php
+++ inc/hero.php
@@ -4,7 +4,7 @@
           <div id="hero_engage">Engage your Audience</div>
           <div id="hero_separate"></div>
           <div id="hero_with">With Live Social Feeds</div>
-          <a id="hero_start" href="#">START FREE</a>
+          <a id="hero_start" href="./#start">START FREE</a>
           <div class="spacer"></div>
         </div>
         <div class="spacer"></div>
